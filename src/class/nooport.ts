d379a608baad5f5ae73a062fbf4517fd9b2ff985
Alexey <pytely@gmail.com>
2018-05-17 10:01:27 +0300

    Thursday morning cleanup

--- src/class/nooport.ts
+++ src/class/nooport.ts
@@ -2,8 +2,6 @@ import * as SerialPort from 'serialport'
 import { params } from '../etc/params'
 var serport = params.serport
 var baudrate = params.baudrate
-
-
 const template = [171, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 172]
 
 //------------------------------------------------------------------------------
@@ -22,33 +20,32 @@ class NooPort {
   }
   //---------- Установка соединения с со Свистком по COM порту -----------------
   connect(): void {
+    let cmnd = template.slice()
+
+    // cmnd[15] = 4
     if (this.serport != undefined) { return }
     this.serport = new SerialPort(this.port,
 	     { baudRate: baudrate, parser: SerialPort.parsers.byteLength(17) } )
     if (this.serport) {
       // this.serport.on("data", (resp) => { return })
       this.serport.on("error", (resp) => {
-        console.log("SerError:");
+        console.log("SerError:")
         console.log(resp);
       })
-      let cmnd = [171, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 172]
-      let buffer = Uint8Array.from(cmnd)
-      this.serport.write(buffer, (error) => {
+      // let cmnd = [171, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 172]
+      cmnd[1] = 4
+      checksum(cmnd)
+      const buffer = Uint8Array.from(cmnd)
+      this.serport.write(buffer, (err) => {
         this.busy = true
-        if (error) {
-          console.log("Write error: "+ error)
-        }
+        if (err) { console.log("Write error: "+ err) }
       })
-      this.serport.drain( (error) => {
+      this.serport.drain( (err) => {
         this.busy = false
-        if (error) {
-          console.log("Drain error: "+ error)
-        }
+        if (err) { console.log("Drain error: "+ err) }
       })
     }
-    else {
-      console.log("Cannot open " + this.port)
-    }
+    else { console.log("Cannot open " + this.port) }
   }
   //------------------------------------------------------------------------------
   command(addr:number, command:number) { this.send(WriteCmnd(addr, command)) }
@@ -75,19 +72,22 @@ function send(noo: NooPort, cmnd: Array<number>) {
       noo.busyCounter++
       return
     }
-    else {
-      noo.busyCounter = 0
-    }
+    else { noo.busyCounter = 0 }
+
     const buffer = Uint8Array.from(cmnd)
     noo.serport.write(buffer, (error) => {
       noo.busy = true
       if (error) console.log('Serial write error: ' + error)
     })
     noo.serport.drain( (error) => {
-      if (error) console.log('Serial drain error: ' + error)
       noo.busy = false
+      if (error) console.log('Serial drain error: ' + error)
     })
   }
+}
+//------------------------------------------------------------------------------
+function checksum(cmnd): void {
+  for (let i=0; i<15; i++) { cmnd[15] += cmnd[i] }
 }
 //------------- Команда управления нагрузкой -----------------------------------
 // 0  - выключить
@@ -97,9 +97,10 @@ function send(noo: NooPort, cmnd: Array<number>) {
 function WriteCmnd(addr:number, command:number): Array<number> {
   let cmnd = template.slice()
 
-  cmnd[4] = +addr
-  cmnd[5] = +command
-  for (let i=0; i<15; i++) { cmnd[15] += cmnd[i] }
+  cmnd[4] = Number(addr)
+  cmnd[5] = Number(command)
+  checksum(cmnd)
+  // for (let i=0; i<15; i++) { cmnd[15] += cmnd[i] }
   return cmnd
 }
 //---------- Команда режим актвации привязки  ----------------------------------
@@ -107,7 +108,9 @@ function ActivateCmnd(addr:number): Array<number> {
   let cmnd = template.slice()
 
   cmnd[2] = 3
-  cmnd[4] = +addr
+  cmnd[4] = Number(addr)
+  checksum(cmnd)
+  // for (let i=0; i<15; i++) { cmnd[15] += cmnd[i] }
   for (let i=0; i<15; i++) { cmnd[15] += cmnd[i] }
   return cmnd
 }
